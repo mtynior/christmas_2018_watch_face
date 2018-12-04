f06a32ec1e4775d9d34c2757be6f3243f2b21158
Michal Tynior <michal.git@tynior.com>
2018-12-04 18:55:05 +0100

    Initial Commit

---
+++ Watch_Face_Christmas_2018/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Watch_Face_Christmas_2018
+//
+//  Created by Michal on 04/12/2018.
+//  Copyright © 2018 Michal Tynior. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
